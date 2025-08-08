44aba9670bfdac00de1a205ca1bb4a28a4828a6c
invalidunits <invalidunits@gmail.com>
2025-08-08 14:46:02 -0400

    Do use Packets -.-

---
+++ Packet.cs
@@ -0,0 +1,95 @@
+using System;
+using System.IO;
+using System.Net;
+
+namespace RainMeadow.Shared
+{
+    public abstract class Packet
+    {
+        public enum Type : byte
+        {
+            None,
+
+            // LAN stuff
+            RequestJoin,
+            JoinLobby,
+            ModifyPlayerList,
+            Session,
+            SessionEnd,
+            RequestLobby,
+            InformLobby,
+            ChatMessage,
+
+            // Router stuff
+        }
+
+        public delegate void BuildPacket_t(Type type, ref Packet? packet);
+        public static event BuildPacket_t? packetFactory;
+
+        public abstract Type type { get; }
+        public ushort size = 0;
+
+        public virtual void Serialize(BinaryWriter writer) { } // Write into bytes
+        public virtual void Deserialize(BinaryReader reader) { } // Read from bytes
+        public virtual void Process() { } // Do the payload
+
+        public IPEndPoint processingEndpoint;
+
+
+
+
+        public static void Encode(Packet packet, BinaryWriter writer, IPEndPoint toEndpoint)
+        {
+            packet.processingEndpoint = toEndpoint;
+
+            writer.Write((byte)packet.type);
+            long payloadPos = writer.Seek(2, SeekOrigin.Current);
+
+
+            packet.Serialize(writer);
+            packet.size = (ushort)(writer.BaseStream.Position - payloadPos);
+
+            writer.Seek((int)payloadPos - 2, SeekOrigin.Begin);
+            writer.Write(packet.size);
+            writer.Seek(packet.size, SeekOrigin.Current);
+        }
+
+        public static void Decode(BinaryReader reader, IPEndPoint fromEndpoint)
+        {
+            Type type = (Type)reader.ReadByte();
+            // RainMeadow.Debug($"Recieved {type}");
+            //RainMeadow.Debug("Got packet type: " + type);
+
+            Packet? packet = null;
+            packetFactory?.Invoke(type, ref packet);
+            
+
+
+            if (packet == null)
+            {
+                // throw new Exception($"Undetermined packet type ({type}) received");
+                RainMeadow.Error("Bad Packet Type Recieved");
+                return;
+            } 
+            
+            packet.processingEndpoint = fromEndpoint;
+            packet.size = reader.ReadUInt16();
+            var startingPos = reader.BaseStream.Position;
+
+            try
+            {
+                packet.Deserialize(reader);
+                var readLength = reader.BaseStream.Position - startingPos;
+
+                if (readLength != packet.size) throw new Exception($"Payload size mismatch, expected {packet.size} but read {readLength}");
+
+                packet.Process();
+            }
+            finally
+            {
+                // Move stream position to next part of packet
+                reader.BaseStream.Position = startingPos + packet.size;
+            }
+        }
+    }
+}
