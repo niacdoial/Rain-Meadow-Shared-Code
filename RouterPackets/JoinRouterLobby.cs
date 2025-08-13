dee93aada2588bf76a1ef007a27d355976b7c1d2
invalidunits <invalidunits@gmail.com>
2025-08-13 18:28:15 -0400

    JoinRouterLobby packet

--- RouterPackets/JoinRouterLobby.cs
+++ RouterPackets/JoinRouterLobby.cs
@@ -6,16 +6,24 @@ namespace RainMeadow.Shared
 {
     public class JoinRouterLobby : Packet
     {
+        public ushort yourRoutingID;
+        public JoinRouterLobby() { }
+
+        public JoinRouterLobby(ushort yourRoutingID)
+        {
+            this.yourRoutingID = yourRoutingID;
+        }
+
         public override Type type => Type.JoinRouterLobby;
             
         public override void Serialize(BinaryWriter writer)
         {
-
+            writer.Write(yourRoutingID);
         } 
         
         public override void Deserialize(BinaryReader reader)
         {
-
+            yourRoutingID = reader.ReadUInt16();
         }
 
         static public event Action<JoinRouterLobby>? ProcessAction = null;
