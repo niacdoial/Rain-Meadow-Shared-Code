1f3b9af0e7cde5b36e4201d1e23efded66854a98
invalidunits <invalidunits@gmail.com>
2025-08-13 23:25:14 -0400

    Change JoinRouterLobby packet

--- RouterPackets/JoinRouterLobby.cs
+++ RouterPackets/JoinRouterLobby.cs
@@ -6,25 +6,57 @@ namespace RainMeadow.Shared
 {
     public class JoinRouterLobby : Packet
     {
+        public int currentplayercount = default;
+        public int maxplayers = default;
+        public bool passwordprotected = default;
+        public string name = "";
+        public string mode = "";
+        public string mods = "";
+        public string bannedMods = "";
         public ushort yourRoutingID;
-        public JoinRouterLobby() { }
 
-        public JoinRouterLobby(ushort yourRoutingID)
+        public JoinRouterLobby() { }
+        public JoinRouterLobby(ushort yourRoutingID, int maxplayers, string name, bool passwordprotected, string mode, int currentplayercount, string highImpactMods = "", string bannedMods = "")
         {
+            this.maxplayers = maxplayers;
+            this.name = name;
+            this.passwordprotected = passwordprotected;
+            this.mode = mode;
+            this.currentplayercount = currentplayercount;
+            this.mods = highImpactMods;
+            this.bannedMods = bannedMods;
+
             this.yourRoutingID = yourRoutingID;
         }
 
         public override Type type => Type.JoinRouterLobby;
-            
+
         public override void Serialize(BinaryWriter writer)
         {
+            base.Serialize(writer);
+            writer.Write(maxplayers);
+            writer.Write(currentplayercount);
+            writer.Write(passwordprotected);
+            writer.Write(name);
+            writer.Write(mode);
+            writer.Write(mods);
+            writer.Write(bannedMods);
             writer.Write(yourRoutingID);
-        } 
-        
+        }
+
         public override void Deserialize(BinaryReader reader)
         {
+            base.Deserialize(reader);
+            maxplayers = reader.ReadInt32();
+            currentplayercount = reader.ReadInt32();
+            passwordprotected = reader.ReadBoolean();
+            name = reader.ReadString();
+            mode = reader.ReadString();
+            mods = reader.ReadString();
+            bannedMods = reader.ReadString();
             yourRoutingID = reader.ReadUInt16();
         }
+
 
         static public event Action<JoinRouterLobby>? ProcessAction = null;
         public override void Process()
